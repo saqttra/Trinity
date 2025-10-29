92e1a433220a21da026fd6f398f40934d8d2acb7
saqttra <113034629+saqttra@users.noreply.github.com>
2025-10-28 23:46:39 -0600

    chore [v 0.1.5-beta] master: fix typo in comment header

--- termrain.rb
+++ termrain.rb
@@ -5,7 +5,7 @@
 # This source code is licensed under the MIT license
 # found in the LICENSE file in the root directory.
 
-# <https://github.com/saqttra/Termrain>.
+# <https://github.com/saqttra/termrain>.
 
 # frozen_string_literal: true
 
