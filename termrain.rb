683015eabc02ef4c4a36cebf326c654c395396b5
saqttra <113034629+saqttra@users.noreply.github.com>
2025-10-28 23:44:06 -0600

    chore [v 0.1.3-beta] master: update copyright notice in comment header

--- termrain.rb
+++ termrain.rb
@@ -1,6 +1,6 @@
 # Copyright (C) 2025, Saqttra. All rights reserved.
 
-# This file is part of Saqttra Trinity.
+# This file is part of Saqttra Termrain.
 
 # This source code is licensed under the MIT license
 # found in the LICENSE file in the root directory.
